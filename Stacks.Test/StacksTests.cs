c7eebfc3be4d4e64677fdb04211710fc81ad744a
Ramon Quintero <ramonquintero0309@gmail.com>
2021-10-25 15:09:53 -0400

    Primera carga

---
+++ Stacks.Test/StacksTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Stacks;
+
+namespace Stacks.Test
+{
+    [TestFixture]
+    public class StacksTests
+    {
+        
+        [Test]
+        public void Push_Argumento_Nulo_ArrojaExcepcion()
+        {
+            var stack = new Stacks<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+
+
+        }
+    }
+}
---
+++ Stacks/Stacks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    public class Stacks<T>
+    {
+        private readonly List<T> _list = new List<T>();
+
+        public int Count => _list.Count;
+
+        public void Push(T obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+
+            _list.Add(obj);
+        }
+
+    }
+}
